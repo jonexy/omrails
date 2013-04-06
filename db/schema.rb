5ee85f0afcdecdf4871f52eb3d76f8b9d7a59e0f
Jonex Ybanez <jonex_ybanez@yahoo.com>
2013-04-06 22:37:26 +0800

    Add Image Url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130330162918) do
+ActiveRecord::Schema.define(:version => 20130406142644) do
 
   create_table "installs", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20130330162918) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "image_remote_url"
   end
 
   add_index "pins", ["user_id"], :name => "index_pins_on_user_id"
